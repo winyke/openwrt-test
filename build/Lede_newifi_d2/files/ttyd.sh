2c95d564d33c520020eadf0ae84b9319d1672a5c
winyke <74634438+winyke@users.noreply.github.com>
2020-12-06 14:57:24 +0800

    Add files via upload

---
+++ build/Lede_newifi_d2/files/ttyd.sh
@@ -0,0 +1,10 @@
+# !/bin/sh
+ln -s /usr/lib/libwebsockets.so.15 /usr/lib/libwebsockets.so.14
+ln -s /usr/lib/libjson-c.so.4 /usr/lib/libjson-c.so.2
+cp etc_cf_ttyd /etc/config/ttyd
+cp etc_initd_ttyd /etc/init.d/ttyd
+cp usr_bin_ttyd /usr/bin/ttyd
+chmod +x /etc/init.d/ttyd
+chmod +x /usr/bin/ttyd
+/etc/init.d/ttyd enable
+/etc/init.d/ttyd start
