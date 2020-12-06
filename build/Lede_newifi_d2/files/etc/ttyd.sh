36c8297c3f728b9b1f75b1ba08eca933b8b92f29
winyke <74634438+winyke@users.noreply.github.com>
2020-12-06 20:59:23 +0800

    Update ttyd.sh

--- build/Lede_newifi_d2/files/etc/ttyd.sh
+++ build/Lede_newifi_d2/files/etc/ttyd.sh
@@ -1,10 +1,11 @@
 # !/bin/sh
 ln -s /usr/lib/libwebsockets.so.15 /usr/lib/libwebsockets.so.14
 ln -s /usr/lib/libjson-c.so.4 /usr/lib/libjson-c.so.2
-cp etc_cf_ttyd /etc/config/ttyd
-cp etc_initd_ttyd /etc/init.d/ttyd
-cp usr_bin_ttyd /usr/bin/ttyd
+mv /etc/etc_cf_ttyd /etc/config/ttyd
+mv /etc/etc_initd_ttyd /etc/init.d/ttyd
+mv /etc/usr_bin_ttyd /usr/bin/ttyd
 chmod +x /etc/init.d/ttyd
 chmod +x /usr/bin/ttyd
 /etc/init.d/ttyd enable
 /etc/init.d/ttyd start
+rm -f /etc/ttyd.sh
